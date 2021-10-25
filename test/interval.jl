9477cbfa8f6cca910628bf3456cd59b758138624
Junxiao Hou <junxiaohou@gmail.com>
2021-10-25 00:39:05 -0500

    Tests are added for question 1 in interval.jl

--- test/interval.jl
+++ test/interval.jl
@@ -3,4 +3,9 @@ using Test
 
 @testset "interval.jl" begin
     
+    @testset "Q1" begin
+        iv1 = Interval(-5.34, 9.78)
+        @test minimum(iv1) == -5.34
+        @test maximum(iv1) == 9.78
+    end
 end
