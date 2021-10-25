0db1cfe75f581c3f9871a3cb2410b8bc79ea6371
Junxiao Hou <junxiaohou@gmail.com>
2021-10-25 01:12:50 -0500

    Tests for question 3 in interval.jl are added

--- test/interval.jl
+++ test/interval.jl
@@ -3,7 +3,7 @@ using Test
 
 @testset "interval.jl" begin
     
-    iv1 = Interval(-5.34, 9.78)
+    iv1 = Interval(-5.34, 9.78) # A non-empty closed interval
     
     @testset "Q1" begin
         @test minimum(iv1) == -5.34
@@ -18,4 +18,10 @@ using Test
         @test ! (9.79 ∈ iv1)
     end
 
+    iv2 = Interval(9.78,-5.34) # An empty closed interval
+
+    @testset "Q3" begin
+        @test !isempty(iv1)
+        @test isempty(iv2)
+    end
 end
