f60d97714ebfde5d1845a3bef728dfb7b461a88b
Junxiao Hou <junxiaohou@gmail.com>
2021-10-25 00:57:00 -0500

    Tests for question 2 in interval.jl are added

--- test/interval.jl
+++ test/interval.jl
@@ -3,9 +3,17 @@ using Test
 
 @testset "interval.jl" begin
     
+    iv1 = Interval(-5.34, 9.78)
+    
     @testset "Q1" begin
-        iv1 = Interval(-5.34, 9.78)
         @test minimum(iv1) == -5.34
         @test maximum(iv1) == 9.78
     end
+
+    @testset "Q2" begin
+        @test 0 ∈ iv1
+        @test -5.34 ∈ iv1
+        @test -9.78 ∈ iv1
+    end
+
 end
